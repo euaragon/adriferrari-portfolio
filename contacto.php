823deb229525d5cd7125d3875909850b5fbde350
Eugenio Aragón <97991009+euaragon@users.noreply.github.com>
2023-05-20 19:38:24 -0300

    web ok con cv

--- contacto.php
+++ contacto.php
@@ -1,22 +1,35 @@
+
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $fullname = $_POST["fullname"];
-  $phone = $_POST["phone"];
-  $email = $_POST["email"];
-  $message = $_POST["message"];
+if(isset($_POST['email'])) {
 
-  // Aquí puedes agregar tu lógica para procesar los datos del formulario
-  // Por ejemplo, enviar un correo electrónico o guardar la información en una base de datos
+// Debes editar las próximas dos líneas de código de acuerdo con tus preferencias
+$email_to = "eugenio.aragon@gmail.com";
+$email_subject = "Contacto desde el sitio web";
 
-  // Ejemplo de envío de correo electrónico
-  $to = "eugenio.aragon@gmail.com";
-  $subject = "Nuevo mensaje del formulario de contacto";
-  $email_body = "Nombre: $fullname\nTeléfono: $phone\nEmail: $email\nMensaje:\n$message";
-  $headers = "From: $email";
-  mail($to, $subject, $email_body, $headers);
+// Aquí se deberían validar los datos ingresados por el usuario
+if(!isset($_POST['fullname']) ||
+!isset($_POST['email']) ||
+!isset($_POST['phone']) ||
+!isset($_POST['message'])) {
 
-  // Redirigir al usuario a una página de éxito o mostrar un mensaje de confirmación
-  header("Location: index.html");
-  exit();
+echo "<b>Ocurrió un error y el formulario no ha sido enviado. </b><br />";
+echo "Por favor, vuelva atrás y verifique la información ingresada<br />";
+die();
+}
+
+$email_message = "Detalles del formulario de contacto:\n\n";
+$email_message .= "Nombre y Apellido: " . $_POST['fullname'] . "\n";
+$email_message .= "E-mail: " . $_POST['email'] . "\n";
+$email_message .= "Teléfono: " . $_POST['phone'] . "\n";
+$email_message .= "Mensaje: " . $_POST['message'] . "\n\n";
+
+// Ahora se envía el e-mail usando la función mail() de PHP
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+mail($email_to, $email_subject, $email_message, $headers);
+echo '<script>alert("El formulario se está enviando");</script>';
+
+echo "¡El formulario se ha enviado con éxito!";
 }
 ?>
